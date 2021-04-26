5563544591acb5a5584fbadaa267fbc62157d512
joergboe <joergboe@de.ibm.com>
2021-04-26 15:43:18 +0200

    Add ProjectInPlace

---
+++ ProjectInPlace/m1.cpp
@@ -0,0 +1,11 @@
+#include "m2.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World Program m1 !!!" << endl;
+	hellom2();
+	return 0;
+}
---
+++ ProjectInPlace/m2.h
@@ -0,0 +1,8 @@
+#ifndef M2_HPP_
+#define M2_HPP_
+
+#include "definitions.h"
+
+void hellom2();
+
+#endif /* M2_HPP_ */
