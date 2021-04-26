2d9d93403d7b7c5cffc3168ba55121d68401977a
joergboe <joergboe@de.ibm.com>
2021-04-26 21:13:43 +0200

    Add ProjectOutPlaceBuild

---
+++ ProjectOutPlaceBuild/include/defines/definitions.h
@@ -0,0 +1,6 @@
+#ifndef DEFINITIONS_HPP_
+#define DEFINITIONS_HPP_
+
+const int maxcount = 3;
+
+#endif /* DEFINITIONS_HPP_ */
---
+++ ProjectOutPlaceBuild/src/m2.cpp
@@ -0,0 +1,13 @@
+#include "m2.h"
+#include "defines/definitions.h"
+
+#include <iostream>
+
+using namespace std;
+
+void hellom2() {
+	for (int i = 0; i < maxcount; ++i) {
+		cout << "Hello World Program m2 !!!" << endl;
+	}
+	return;
+}
