7c1358aad9758ec8da1bea2137355b8868c9d0e7
Joerg Boehmer <joerg.boehmer@snafu.de>
2024-09-30 14:21:45 +0200

    Switch to testframework v0.0.3

--- Test/settings.sh
+++ Test/settings.sh
@@ -1,3 +1,4 @@
 # Constants for test framework installation
-export TESTFRAMEWORK_INSTALL_SCRIPT='testframeInstaller_v0.0.1.sh'
-export TESTFRAMEWORK_SOURCE="https://github.com/joergboe/runbtf/releases/download/v0.0.1/${TESTFRAMEWORK_INSTALL_SCRIPT}"
+export TESTFRAMEWORK_INSTALL_DOWNLOAD='testframeInstaller_v0.0.3.sh.gz'
+export TESTFRAMEWORK_INSTALL_SCRIPT="${TESTFRAMEWORK_INSTALL_DOWNLOAD%.gz}"
+export TESTFRAMEWORK_SOURCE="https://github.com/joergboe/runbtf/releases/download/v0.0.3/${TESTFRAMEWORK_INSTALL_DOWNLOAD}"
