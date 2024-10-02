c0904997a36b7384ed0f8ca38cd91f6713c8ef80
Joerg Boehmer <joerg.boehmer@snafu.de>
2024-10-02 21:12:27 +0200

    New stype OneToOne

--- CommonCustomization/warnings.g++11.mk
+++ CommonCustomization/warnings.g++11.mk
@@ -3,7 +3,7 @@
 # or provide a link makefile.warn pointing to this file : ln -s ../CommonCustomization/warnings.g++11.mk warnings.mk
 
 cxxwarn2 = -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations\
--Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wconversion\
--Wsign-conversion -Wstrict-null-sentinel -Wundef -Wfloat-equal -Winline -Wzero-as-null-pointer-constant\
--Wuseless-cast -Wstrict-overflow=4 -Wattribute-alias -Wduplicated-branches -Wduplicated-cond -Wdate-time -Wextra-semi\
--Wnull-dereference -Wno-aggressive-loop-optimizations -Wdisabled-optimization
+  -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wconversion\
+  -Wsign-conversion -Wstrict-null-sentinel -Wundef -Wfloat-equal -Winline -Wzero-as-null-pointer-constant\
+  -Wuseless-cast -Wstrict-overflow=4 -Wattribute-alias -Wduplicated-branches -Wduplicated-cond -Wdate-time -Wextra-semi\
+  -Wnull-dereference -Wno-aggressive-loop-optimizations -Wdisabled-optimization
